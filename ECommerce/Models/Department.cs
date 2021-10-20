f9c9ebc831364f88be1f66e10ce0a7b6ac8cdb3d
saviorarauto <magno2006.diego@gmail.com>
2021-10-19 21:21:16 -0300

    First_Commit

---
+++ ECommerce/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        [Required(ErrorMessage ="Field is required!")]
+        public string Name { get; set; }
+    }
+}
---
+++ ECommerce/Models/ECommerceContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ECommerce.Models
+{
+    public class ECommerceContext : DbContext
+    {
+        public ECommerceContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
